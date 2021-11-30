308b659fb2c6e755fca0463d4fce6fc279f57cff
Raaj Patil <raaj.2501@gmail.com>
2021-11-29 17:25:47 -0800

    Add space efficient alignment functions

--- 5840387942_6161762888_efficient.py
+++ 5840387942_6161762888_efficient.py
@@ -95,6 +95,46 @@ def align_strings(s1, s2):
     return dp[-1][-1], "".join(s1_aligned), "".join(s2_aligned)
 
 
+def space_efficient_alignment(s1, s2):
+    dp = [[0, 0] for _ in range(len(s1) + 1)]
+
+    for i in range(len(s1) + 1):
+        dp[i][0] = i * DELTA
+
+    for i in range(1, len(s1) + 1):
+        dp[0][1] = i * DELTA
+        for j in range(1, len(s2) + 1):
+            dp[j][1] = min(
+                dp[j - 1][0] + ALPHA[s1[i - 1]][s2[j - 1]],
+                dp[j - 1][1] + DELTA,
+                dp[j][0] + DELTA,
+            )
+
+        for j in range(len(s1) + 1):
+            dp[j][0] = dp[j][1]
+    return dp[: len(string2) + 1]
+
+
+def backward_space_efficient_alignment(s1, s2):
+    dp = [[0, 0] for _ in range(len(s1) + 1)]
+
+    for i in range(len(s1) + 1):
+        dp[i][1] = i * DELTA
+
+    for i in range(1, len(s1) + 1):
+        dp[0][0] = i * DELTA
+        for j in range(1, len(s2) + 1):
+            dp[j][0] = min(
+                dp[j - 1][1] + ALPHA[s1[i - 1]][s2[j - 1]],
+                dp[j - 1][0] + DELTA,
+                dp[j][1] + DELTA,
+            )
+
+        for j in range(len(s1) + 1):
+            dp[j][1] = dp[j][0]
+    return dp[: len(string2) + 1]
+
+
 def write_output_file(
     s1, s2, solution_cost, time_taken, memory_used, filename="output.txt"
 ):

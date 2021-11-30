b3739b27c4e643d1671b9ce9034ef26164ae1c6d
Raaj Patil <raaj.2501@gmail.com>
2021-11-29 18:10:41 -0800

    Fix backward space efficient indexing

--- 5840387942_6161762888_efficient.py
+++ 5840387942_6161762888_efficient.py
@@ -125,7 +125,7 @@ def backward_space_efficient_alignment(s1, s2):
         dp[0][0] = i * DELTA
         for j in range(1, len(s2) + 1):
             dp[j][0] = min(
-                dp[j - 1][1] + ALPHA[s1[i - 1]][s2[j - 1]],
+                dp[j - 1][1] + ALPHA[s1[len(s1) - i]][s2[len(s2) - j]],
                 dp[j - 1][0] + DELTA,
                 dp[j][1] + DELTA,
             )
